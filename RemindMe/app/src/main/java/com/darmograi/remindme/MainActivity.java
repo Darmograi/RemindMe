dcc5a7e1ee7140a06bf2a804bf16a3eddbb03851
Oleksandr Husliakov <darmograi78@gmail.com>
2016-03-18 20:53:17 +0200

    Created MainActivity.java and activity_main.xml

---
+++ RemindMe/app/src/main/java/com/darmograi/remindme/MainActivity.java
@@ -0,0 +1,16 @@
+package com.darmograi.remindme;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Oleksandr Husliakov on 18.03.2016.
+ */
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
